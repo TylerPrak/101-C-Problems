a42a7c923b53cb73622ef150fc9a44442ca3d19f
root <prayforparasites@gmail.com>
2017-03-29 03:07:39 -0400

    Added problem 5. First push from ssh'ing into my server on my phone.

---
+++ Problems/1-10/05_Sum_Of_Two_Values.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int 	main (int argc, char *argv[]) {
+	int num1, num2, sum;
+
+	printf ("Please enter an integer.\n");
+	scanf ("%d", &num1);
+	printf ("Please enter another integer.\n");
+	scanf ("%d", &num2);
+
+	sum = num1 + num2;
+    printf ("The sum of the two integers you entered is %d.\n", sum);
+
+	return 0;
+}
