74cd2e7fdec2425b6c030bb39781f158e411069d
root <prayforparasites@gmail.com>
2017-03-30 22:24:58 -0400

    Added problem 6 and file that the problem reads using fscanf,fopen and fclose.

---
+++ Problems/1-10/06_The_fscanf_Function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int		main (int argc, char *argv[]) {
+
+	int num;
+	
+	FILE * fp;
+	fp = fopen ("testdata6", "r");
+	fscanf(fp,"%d", &num);
+
+	printf("%d\n", num);
+
+	fclose(fp);
+	return 0;
+}
