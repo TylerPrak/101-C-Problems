df894d8f68f232c0f6b6cbe998f1e78be57a4abb
root <prayforparasites@gmail.com>
2017-04-02 22:32:05 -0400

    Added Problem 25.

---
+++ Problems/25_Sum_of_A_Bunch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int		main(int argc, char *argv) {
+
+	int num, sum;
+	
+	FILE * fp;
+	fp = fopen("../Testdata/testdata25", "r");
+
+	while (fscanf(fp, "%d", &num) != EOF) {
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+	return 0;
+}
