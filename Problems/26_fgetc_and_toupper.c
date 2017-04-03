8907fa45bc87ec1fbb045bf5e072d6b4bf57f8ae
root <prayforparasites@gmail.com>
2017-04-02 23:05:33 -0400

    Added Problem 26.

---
+++ Problems/26_fgetc_and_toupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h> 
+
+int		main(int argc, char *argv[]) {
+
+	int c;
+
+	FILE * fp;
+	fp = fopen("../Testdata/testdata26", "r");
+
+	while ((c = fgetc(fp)) != EOF)
+		printf("%c", toupper(c));
+	return 0;
+}  
